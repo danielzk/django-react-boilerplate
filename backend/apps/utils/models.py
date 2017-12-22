08f875e209f4b5d3f6eb8b8105f77e87b0c448f5
Daniel Ramos <danielrz@protonmail.com>
2017-12-22 04:32:09 -0300

    updated_at must be null when created

--- backend/apps/utils/models.py
+++ backend/apps/utils/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.db.models import DateTimeField
+from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
 
 class TimeStampedModel(models.Model):
     created_at = DateTimeField(_('Created at'), auto_now_add=True)
-    updated_at = DateTimeField(_('Updated at'), auto_now=True)
+    updated_at = DateTimeField(_('Updated at'), null=True, editable=False)
 
     class Meta:
         abstract = True
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.updated_at = now()
+        super().save(*args, **kwargs)
 
 
 class ValidationModel(models.Model):
