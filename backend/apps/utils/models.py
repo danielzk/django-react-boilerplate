4cdea86ea2d72c9aa8c0e159d6748ec5fcc38f0b
Daniel Ramos <danielrz@protonmail.com>
2017-12-22 06:47:50 -0300

    Adapt models to django-polymorphic

--- backend/apps/utils/models.py
+++ backend/apps/utils/models.py
@@ -3,29 +3,44 @@ from django.db.models import DateTimeField
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
+from polymorphic import models as polymorphic_models
 
-class TimeStampedModel(models.Model):
-    created_at = DateTimeField(_('Created at'), auto_now_add=True)
-    updated_at = DateTimeField(_('Updated at'), null=True, editable=False)
 
-    class Meta:
-        abstract = True
+class ValidationModelMixin(object):
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
+
+class TimeStampedSaveModelMixin(object):
     def save(self, *args, **kwargs):
         if self.pk:
             self.updated_at = now()
         super().save(*args, **kwargs)
 
 
-class ValidationModel(models.Model):
+class TimeStampedModel(TimeStampedSaveModelMixin, models.Model):
+    created_at = DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at = DateTimeField(_('Updated at'), null=True, editable=False)
+
     class Meta:
         abstract = True
 
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
+
+class BaseModel(ValidationModelMixin, TimeStampedModel):
+    class Meta:
+        abstract = True
 
 
-class BaseModel(TimeStampedModel, ValidationModel):
+class TimeStampedPolymorphicModel(TimeStampedSaveModelMixin,
+                                  polymorphic_models.PolymorphicModel):
+    created_at = DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at = DateTimeField(_('Updated at'), null=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class BasePolymorphicModel(ValidationModelMixin, TimeStampedPolymorphicModel):
     class Meta:
         abstract = True
