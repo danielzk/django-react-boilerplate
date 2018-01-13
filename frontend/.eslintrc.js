dc0e2d004d29d20886171b22bf15574a866fe120
Daniel Ramos <danielrz@protonmail.com>
2018-01-13 01:55:00 -0300

    Fix indentation

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true,
-        "jest": true,
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "jest": true,
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "experimentalDecorators": true,
+      "jsx": true,
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "experimentalDecorators": true,
-            "jsx": true,
-        },
-        "sourceType": "module",
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2,
-        ],
-        "linebreak-style": [
-            "error",
-            "unix",
-        ],
-        "quotes": [
-            "error",
-            "single",
-            {"allowTemplateLiterals": true},
-        ],
-        "semi": [
-            "error",
-            "never",
-        ],
-        "react/prop-types": "off",
-    },
+    "sourceType": "module",
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+    ],
+    "linebreak-style": [
+      "error",
+      "unix",
+    ],
+    "quotes": [
+      "error",
+      "single",
+      {"allowTemplateLiterals": true},
+    ],
+    "semi": [
+      "error",
+      "never",
+    ],
+    "react/prop-types": "off",
+  },
 }
